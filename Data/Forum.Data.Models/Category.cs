2bcf0f1dd903839282740c264502a001e51368ea
plamen <plamen91ivanov@abv.bg>
2020-07-28 21:35:57 +0300

    Migrations

--- Data/Forum.Data.Models/Category.cs
+++ Data/Forum.Data.Models/Category.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Forum.Data.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Forum.Data.Models
 {
-    class Category
+    public class Category : BaseDeletableModel<int>
     {
+        public Category()
+        {
+            this.Posts = new HashSet<Post>();
+        }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
     }
 }
