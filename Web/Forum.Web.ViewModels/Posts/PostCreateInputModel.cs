27218772addfaad54f1f04ada07f7041380aced5
plamen <plamen91ivanov@abv.bg>
2020-08-19 10:01:48 +0300

    Remove button functionality

---
+++ Web/Forum.Web.ViewModels/Posts/PostCreateInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forum.Web.ViewModels.Posts
+{
+    public class PostCreateInputModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Web/Forum.Web/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Web.Controllers
+{
+    public class PostsController : Controller
+    {
+        public PostsController()
+        {
+
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
