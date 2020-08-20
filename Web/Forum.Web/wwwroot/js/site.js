21b883a5fb1eb98ad64521635ce0314f50107f55
plamen <plamen91ivanov@abv.bg>
2020-08-20 13:50:30 +0300

    js

--- Web/Forum.Web/wwwroot/js/site.js
+++ Web/Forum.Web/wwwroot/js/site.js
@@ -1,14 +1,13 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded',ready)
-}else{
+// Write your JavaScript code.if (document.readyState == 'loading') {
+document.addEventListener('DOMContentLoaded', ready)
+}else {
     ready()
 }
 
-function ready(){
+function ready() {
 
     var removeCardItemButton = document.getElementsByClassName('btn-danger');
     console.log(removeCardItemButton);
@@ -17,23 +16,24 @@ function ready(){
         button.addEventListener('click', removeCartItem)
     }
     var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++){
+    for (var i = 0; i < quantityInputs.length; i++) {
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
 
     var addToCartButtons = document.getElementsByClassName('shop-item-button')
-        for (let i = 0; i < addToCartButtons.length; i++) {
-            var button = addToCartButtons[i]
-            button.addEventListener('click', addToCardClicked)
-        }
- 
+    for (let i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCardClicked)
+        console.log('addToCardClicked')
+    }
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click',purchaseClicked)
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
- 
- 
-function purchaseClicked(){
+
+
+function purchaseClicked() {
     alert('Thank you for your purchase')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
@@ -42,17 +42,19 @@ function purchaseClicked(){
     updateCartTotal()
 }
 
-function addToCardClicked(event){
+function addToCardClicked(event) {
     var button = event.target
     var shopItem = button.parentElement.parentElement
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc =shopItem.getElementsByClassName('shop-item-image')[0].src 
-    addItemToCard(title,price,imageSrc)
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    var protein = shopItem.getElementsByClassName('shop-item-protein')[0].innerText
+    var fat = shopItem.getElementsByClassName('shop-item-fat')[0].innerText
+    addItemToCard(title, price, imageSrc, protein, fat)
     updateCartTotal()
 }
 
-function addItemToCard(title, price, imageSrc){
+function addItemToCard(title, price, imageSrc, protein, fat) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
     var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -62,13 +64,15 @@ function addItemToCard(title, price, imageSrc){
             alert('This item is already added to the cart')
             return
         }
-        
+
     }
     var cartRowContent = `<div class="cart-item cart-column">
     <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
     <span class="cart-item-title">${title}</span>
 </div>
 <span class="cart-price cart-column">${price}</span>  
+<span class="cart-protein cart-column">${protein}</span>
+<span class="cart-fat cart-column">${fat}</span>
 <div class="cart-quantity cart-column">
     <input class="cart-quantity-input" type="number" value="1">
     <button class="btn btn-danger" type="button">REMOVE</button>
@@ -79,7 +83,7 @@ function addItemToCard(title, price, imageSrc){
     cartRow.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged)
 }
 
-function removeCartItem(event){
+function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
@@ -87,7 +91,7 @@ function removeCartItem(event){
 
 function quantityChanged(event) {
     var input = event.target
-    if (isNaN(input.value ) || input.value <= 0) {
+    if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
     updateCartTotal()
@@ -98,15 +102,25 @@ function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
+    var totalProtein = 0
+    var totalFat = 0
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-       
+        var proteinElement = cartRow.getElementsByClassName('cart-protein')[0]
+        var fatElement = cartRow.getElementsByClassName('cart-fat')[0]
+
         var price = parseFloat(priceElement.innerText.replace('$', ''))
+
         var quantity = quantityElement.value
         total = total + (price * quantity)
+        totalProtein = totalProtein + parseFloat(proteinElement.innerText)
+        totalFat = totalFat + parseFloat(fatElement.innerText)
     }
     total = Math.round(total * 100) / 100
+
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-total-protein')[0].innerText = totalProtein
+    document.getElementsByClassName('cart-total-fat')[0].innerText = totalFat
 }
